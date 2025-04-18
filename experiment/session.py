e8001f2cabbeac4f9bd61483c99811efd40958cb
Ningkai <n.wang@vu.nl>
2025-04-18 14:23:51 +0200

    fix finishing outro

--- experiment/session.py
+++ experiment/session.py
@@ -312,7 +312,7 @@ class SingletonSession(PylinkEyetrackerSession):
         self.trials.append(
             OutroTrial(
                 session=self,
-                trial_nr=ix + 1,
+                trial_nr=ix + 2,
                 phase_durations=[
                     self.settings["durations"].get("blank", 1),
                     0.10,
